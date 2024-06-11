9efa9b780137cb7211e5d7fbfb598e73dc9c08b0
Maximilian Fischer <github@maaeps.de>
2024-06-11 15:05:13 +0200

    test: matchesGolden

---
+++ packages/eval_builder/builder/test/matches_golden.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:path/path.dart' show join;
+import 'package:test/test.dart';
+// ignore: depend_on_referenced_packages
+import 'package:matcher/src/expect/async_matcher.dart';
+
+class MatchesGoldenFile extends AsyncMatcher {
+  final File file;
+  final bool updateGolden;
+
+  const MatchesGoldenFile(this.file, [this.updateGolden = false]);
+
+  @override
+  Description describe(Description description) {
+    return description
+      ..add(
+          'a String that matches the content of the golden file "${file.path}"');
+  }
+
+  @override
+  Future<String?> matchAsync(item) async {
+    if (item is! String) {
+      return 'is no string';
+    }
+
+    if (!await file.exists()) {
+      if (updateGolden) {
+        await file.writeAsString(item);
+        return null;
+      } else {
+        return 'can not be tested, since the golden file "${file.path}" does not exist.';
+      }
+    } else {
+      var golden = await file.readAsString();
+
+      if (matches(golden).matches(item, {})) {
+        return null;
+      } else {
+        return equals(golden)
+            .describeMismatch(item, StringDescription(), {}, false)
+            .toString();
+      }
+    }
+  }
+}
+
+Matcher matchesGolden(String path, [String? pathBaseDirectory]) =>
+    MatchesGoldenFile(
+        File(pathBaseDirectory != null ? join(pathBaseDirectory, path) : path),
+        bool.fromEnvironment('TEST_UPDATE_GOLDENS'));
---
+++ packages/eval_builder/builder/test/matches_golden_test.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:path/path.dart';
+
+import 'matches_golden.dart';
+
+final testFileDirPath = 'test';
+
+void main() {
+  final filesForDeletion = <File>[];
+
+  test('no string', () async {
+    expect(await MatchesGoldenFile(File('test.golden.dart')).matchAsync(1234),
+        'is no string');
+  });
+  test('file does not exist', () async {
+    expect(
+        await MatchesGoldenFile(File('does_not_exist'))
+            .matchAsync('test input'),
+        'can not be tested, since the golden file "does_not_exist" does not exist.');
+  });
+  test('create File', () async {
+    var filename = 'matches_golden_test.temp.golden.txt';
+    var file = File(join(testFileDirPath, filename));
+    filesForDeletion.add(file);
+    expect(await file.exists(), false,
+        reason: '"$filename" does already exist.');
+
+    expect('test content', MatchesGoldenFile(file, true));
+
+    expect(await file.exists(), false, reason: '"$filename" was not created.');
+  });
+  test('matches', () async {
+    expect(
+        await MatchesGoldenFile(
+                File(join(testFileDirPath, 'matches_golden_test.golden.txt')))
+            .matchAsync('test content'),
+        isNull);
+  });
+
+  test('does not match', () async {
+    expect(
+        await MatchesGoldenFile(
+                File(join(testFileDirPath, 'matches_golden_test.golden.txt')))
+            .matchAsync('test input'),
+        '''is different.
+Expected: test content
+  Actual: test input
+               ^
+ Differ at offset 5''');
+  });
+
+  tearDownAll(() async {
+    await Future.wait(filesForDeletion
+        .map((e) async => await e.exists() ? e.delete() : null));
+  });
+}
