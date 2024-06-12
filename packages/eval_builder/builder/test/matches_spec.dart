28f5717227e1f08c60dd35fc1ff55319b47df4eb
Maximilian Fischer <github@maaeps.de>
2024-06-12 23:15:55 +0200

    test: Add allocator property to SpecMatcher

--- packages/eval_builder/builder/test/matches_spec.dart
+++ packages/eval_builder/builder/test/matches_spec.dart
@@ -1,14 +1,19 @@
+import 'package:analyzer/dart/analysis/results.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
 import 'package:test/test.dart';
 
 class SpecMatcher extends CustomMatcher {
-  SpecMatcher(matcher) : super('Spec that is', 'code', matcher);
+  SpecMatcher(matcher, [this.allocator])
+      : super('Spec that is', 'code', matcher);
+
+  final Allocator? allocator;
 
   @override
   Object? featureValueOf(actual) {
     var code = (actual as Spec)
-        .accept(DartEmitter(useNullSafetySyntax: true))
+        .accept(DartEmitter(
+            allocator: allocator ?? Allocator.none, useNullSafetySyntax: true))
         .toString();
 
     try {
@@ -19,4 +24,4 @@ class SpecMatcher extends CustomMatcher {
   }
 }
 
-specMatches(matcher) => SpecMatcher(matcher);
+specMatches(matcher, [allocator]) => SpecMatcher(matcher, allocator);
